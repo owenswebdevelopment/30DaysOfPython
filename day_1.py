d47a1ab2b415ee878a3e79ef535a73fb95420da6
owen newsome <melfordmalichai@gmail.com>
2025-10-04 14:57:31 +0900

    initial commit

---
+++ day_1.py
@@ -0,0 +1,33 @@
+#Check the python version you are using
+#Open the python interactive shell and do the following operations. The operands are 3 and 4.
+#addition(+)
+#subtraction(-)
+#multiplication(*)
+#modulus(%)
+#division(/)
+#exponential(**)
+#floor division operator(//)
+#Write strings on the python interactive shell. The strings are the following:
+#Your name
+#Your family name
+#Your country
+#I am enjoying 30 days of python
+
+print(2+3)
+print(2-4)
+print(4*5)
+print(20%2) #result=0
+print(9/18)
+print(3**6) #3*3*3*3*3*3=729
+print(10//2) #5
+print('Owen')
+print('Newsome')
+print('Jamaica')
+print('I am enjoying 30 days of python')
+
+#data types
+print(type('Owen')) #string
+print(type([1, 2, 3, 4, 5])) #integer
+print(type((1, 2, 3, 2.3, 5))) #list
+print(type({'Owen': 'first name'})) #dictionary
+print(type({.5, 2, 'owen'}))
---
+++ hello.py
@@ -0,0 +1,14 @@
+"""This is the start of a python coding bootcamp"""
+print(2+3)
+print(type({'Owen': 'name'}))
+print(type((2, 3, 4)))
+print(type({2.5, 3.8}))
+print(type({'owen', 'david', 'john'}))
+print(type([1, 3, 4, 5, 6, 7]))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Owen Newsome'))
+print(2+10)
+print(3 - 6)
+print(3*3)
+print(7/14)
+print(3**4) #3*3*3*3=81
