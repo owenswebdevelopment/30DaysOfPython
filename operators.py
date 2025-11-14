e665471716f859c8170e3406e2c70b1e6d0f3111
owen newsome <melfordmalichai@gmail.com>
2025-11-14 19:58:21 +0900

    operators

---
+++ operators.py
@@ -0,0 +1,32 @@
+print("Addition:", 2 + 3)
+print("Subtraction:", 2 - 3)
+print("division:", 2 - 8)
+print("multiplication:", 10 * 8)
+print("Modulus:", 10 % 8) #It gives the reminder
+print("Float:", 3.19) #float
+print("multiplying complex numbers:", (3 + 7j) * (4 -6j))#float
+
+
+
+#Declaring a variable
+
+a = 2
+b = 3
+
+total = a + b
+diff = a - b
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b
+
+# I should have used sum instead of total but sum is a built-in function - try to avoid overriding built-in functions
+print(total) # if you do not label your print with some string, you never know where the result is coming from
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print('a ** b = ', exponential)
