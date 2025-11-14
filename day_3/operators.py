fb7aa0acd530ed842756999560c9ff2e6b330f9b
owen newsome <melfordmalichai@gmail.com>
2025-11-14 23:46:59 +0900

    comparison operator

--- day_3/operators.py
+++ day_3/operators.py
@@ -74,3 +74,25 @@ mass = 75 #kg
 volume = 0.075 #cubic meter
 density = mass / volume
 print(density)
+
+#comparison operator
+print(3 > 2)     # True, because 3 is greater than 2
+print(3 >= 2)    # True, because 3 is greater than 2
+print(3 < 2)     # False,  because 3 is greater than 2
+print(2 < 3)     # True, because 2 is less than 3
+print(2 <= 3)    # True, because 2 is less than 3
+print(3 == 2)    # False, because 3 is not equal to 2
+print(3 != 2)    # True, because 3 is not equal to 2
+print(len('mango') == len('avocado'))  # False
+print(len('mango') != len('avocado'))  # True
+print(len('mango') < len('avocado'))   # True
+print(len('milk') != len('meat'))      # False
+print(len('milk') == len('meat'))      # True
+print(len('tomato') == len('potato'))  # True
+print(len('python') > len('dragon'))   # False
+
+
+print("1 is 1", 1 is 1)
+print("1 is not 1", 1 is not 2)
+print("O in Owen", "o" in "Owen")
+print("4 is 2 ** 2", 4 is 2 ** 2)
